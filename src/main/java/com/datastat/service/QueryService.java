7d800e3d7fdb13261853a1d258de591a522a453f
gouzhonglin <gouzhonglin@huawei.com>
2023-08-15 15:08:21 +0800

    add api /innovation_items

--- src/main/java/com/datastat/service/QueryService.java
+++ src/main/java/com/datastat/service/QueryService.java
@@ -1184,4 +1184,16 @@ public class QueryService {
         CustomPropertiesConfig queryConf = getQueryConf(request);
         return queryDao.getNps(queryConf, body);
     }
+
+    public String queryInnovationItems(HttpServletRequest request, String community) {
+        String key = community.toLowerCase() + "innovationItems";
+        QueryDao queryDao = getQueryDao(request);
+        CustomPropertiesConfig queryConf = getQueryConf(request);
+        String result = (String) redisDao.get(key);
+        if (result == null) {
+            result = queryDao.queryInnovationItems(queryConf);
+            redisDao.set(key, result, redisDefaultExpire);
+        }
+        return result;
+    }
 }
