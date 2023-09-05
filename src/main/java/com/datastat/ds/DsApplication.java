392354cfd7bc1bad1ecb881914fdcb2871e26512
gouzhonglin <gouzhonglin@huawei.com>
2023-09-05 19:19:38 +0800

    addbean

--- src/main/java/com/datastat/ds/DsApplication.java
+++ src/main/java/com/datastat/ds/DsApplication.java
@@ -13,7 +13,9 @@ package com.datastat.ds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.datastat.*"})
@@ -21,6 +23,13 @@ public class DsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DsApplication.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        placeholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
+        return placeholderConfigurer;
     }
 
 }
